1417465e67ad7c810b5ba71a982c4f3c56721511
Camilo Mora <kalufau@hotmil.com>
2021-10-07 22:50:31 -0500

    heroku deploy miveciapi

--- api/index.js
+++ api/index.js
@@ -9,16 +9,22 @@ const auth = require('./components/auth/network');
 const post = require('./components/post/network');
 const errors = require('../network/errors');
 const app = express();
+const cors = require('cors');
 
 const swaggerDoc = require('./swagger.json');
 
 app.use(bodyParser.json());
 
 // ROUTER
+app.use(cors);
+
 app.use('/api/user', user);
 app.use('/api/auth', auth);
 app.use('/api/post', post);
-app.use('/apidoc', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use('/apidoc',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDoc),
+);
 app.use(errors);
 
 const server = app.listen(config.api.port, () => {
